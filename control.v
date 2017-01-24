d3612f49ce6bda2f7170d12fc1710f0ed0b7ac57
oskimura <oskimura@gmail.com>
2017-01-25 08:37:57 +0900

    add control.v

---
+++ control.v
@@ -0,0 +1,64 @@
+`default_nettype none
+module contorol(clk,op,cond,funct,rd,
+
+                 pc_src,
+                 reg_write,
+                 mem_to_reg,
+                 mem_write,
+                 alu_control,
+                 branch,
+                 alu_src,
+                 //flag_write,
+                 imm_src,
+                 reg_src);
+    input clk;
+    input [1:0] op;
+    input [3:0] cond;
+    input [5:0] funct;
+    input [3:0] rd;
+    output pc_src,
+           reg_write,
+           mem_to_reg,
+           mem_write,      
+           branch,
+           alu_src;
+    //output [1:0] flag_write;
+    output [1:0] imm_src;
+    output [1:0] alu_control;
+    output [1:0] reg_src;
+
+
+    //wire [1:0] flag_write;
+    wire pcs;
+    wire reg_w;
+    wire mem_w;
+    wire branch;
+    wire [3:0]alu_flag;
+
+    wire [1:0] flag_write;
+
+    decoder decoder_u(.op(op), 
+    .funct(funct), 
+    .rd(rd),
+
+                       .pcs(pcs),
+                       .reg_w(reg_w),
+                       .mem_w(mem_w),
+                       .mem_to_reg(mem_to_reg),
+                       .alu_src(alu_src),
+                       .imm_src(imm_src),
+                       .reg_src(reg_src),
+                       .alu_control(alu_control),
+                       .flag_w(flag_write));
+
+    cond_logic cond_logic_u(.clk(clk),.pcs(pcs),.reg_w(reg_w),.mem_w(),
+
+                        .flag_w(flag_write),
+                        .cond(cond),
+                        .alu_flag(alu_flag),
+                        .pc_src(pc_src),
+                        .reg_write(reg_write),
+                        .mem_write(mem_write));
+
+
+endmodule
