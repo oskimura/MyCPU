a2224d9b40fec7c6c66c471dd79be5a949b876e1
oskimura <oskimura@gmail.com>
2017-02-11 20:10:38 +0900

    modify multi cycle. add state.

--- decoder.v
+++ decoder.v
@@ -95,3 +95,156 @@ module decoder(op,funct,rd,
     assign pcs = ((rd==4'd15)&reg_w)|branch;
     assign swap = (cmd==4'b0011)?1:0;
 endmodule
+`define Fetch 5'd0
+`define Decode 5'd1
+`define MemAddr 5'd2
+`define ExecuteR 5'd3
+`define ExecuteL 5'd4
+`define Branch 5'd5
+`define MemRead 5'd6
+`define MemWrite 5'd7
+`define AluWB 5'd8
+`define MemWB 5'd9
+
+module fsm(clk,
+reset,
+op,
+funct,
+
+reg_w,
+mem_w,
+ir_write,
+next_pc,
+adr_src,
+result_src,
+alu_src_a,
+alu_src_b,
+branch,
+alu_op);
+
+    input clk, reset;
+    input [1:0] op;
+    input [5:0] funct;
+           
+
+    output reg reg_w,
+    mem_w,
+    ir_write,
+    next_pc,
+    adr_src,
+    
+    alu_src_a,
+    branch,
+    alu_op;
+
+    output reg [1:0] result_src,reg_src,
+    alu_src_b;
+
+    reg [4:0] state,next_state;
+    reg cotrol;
+
+    always @(clk or reset) begin
+        if (reset) state <= `Fetch;
+        else state <= next_state;  
+    end
+   
+    always @(clk) begin
+        case (state)
+            `Fetch:
+                begin
+                    adr_src<=1'b0;
+                    alu_src_a=1'b1;
+                    alu_src_b<=2'b10;
+                    alu_op<=1'b0;
+                    result_src<=2'b10;
+                    ir_write<=1'b1;
+                    next_pc<=1'b1;
+
+                    next_state <= `Decode;
+                end
+            `Decode:
+                begin
+                    alu_src_a <= 1'b1;
+                    alu_src_b <= 2'b10;
+                    alu_op<=1'b0;
+                    result_src<=2'b10;
+
+                    if (op==2'b01) next_state <= `MemAddr;
+                    else if (op==2'b00 && funct[5]==1'b0) next_state <= `ExecuteR;
+                    else if (op==2'b00 && funct[5]==1'b1) next_state <= `ExecuteL;
+                    else if (op==2'b10) next_state <= `Branch;
+                end
+            `MemAddr:
+                begin
+                
+                    alu_src_a <= 1'b0;
+                    alu_src_b <= 2'b01;
+                    alu_op <= 1'b0;
+
+                    if (funct[0]==1'b1) next_state <= `MemRead;
+                    else if (funct[0]==1'b0) next_state <= `MemWrite;
+                    
+                end
+            `ExecuteR:
+               begin
+                    alu_src_a<=1'b0;
+                    alu_src_b<=2'b00;
+                    alu_op<=1'b1;
+
+                    next_state = `AluWB;
+                end
+            `ExecuteL:
+                begin
+                    alu_src_a<=1'b0;
+                    alu_src_b<=2'b01;
+                    alu_op<=1'b1;
+
+                    next_state <= `AluWB;
+                end
+            `Branch:
+                begin
+                    alu_src_a <= 1'b0;
+                    alu_src_b <= 2'b01;
+                    result_src <= 2'b10;
+                    branch <= 1'b1;
+
+                    next_state <= `Fetch;
+                end
+            `MemRead:
+                begin
+                    result_src <= 2'b00;
+                    adr_src <= 1'b1;
+
+                    next_state <= `MemWB;
+                end
+            `MemWrite:
+                begin
+                    reg_src<=2'b00;
+                    adr_src<=1'b1;
+                    mem_w<=1'b1;
+
+                    next_state <= `Fetch;
+                end
+            `AluWB:
+                begin
+                    result_src <= 2'b00;
+                    reg_w <= 1'b1;
+
+                    next_state <= `Fetch;
+                end
+            `MemWB:
+                begin
+                    result_src <= 2'b01;
+                    reg_w<=1'b1;
+
+                    next_state <= `Fetch;
+                end
+            default:
+                next_state <= `Fetch;
+        endcase
+    end
+
+
+    //assign alu_src_a = () 1;
+    //assign {reg_w,mem_w,ir_write,next_pc,adr_src,result_src,alu_src_a,alu_src_b} = control;
+
